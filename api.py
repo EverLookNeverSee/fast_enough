d2575e34659da2e95cf056736e167a510f851bf4
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-02 10:34:18 +0430

    Defining hello router

--- api.py
+++ api.py
@@ -2,12 +2,12 @@
     API for the application.
 """
 
-from fastapi import FastAPI
+from fastapi import APIRouter
 
-# Defining a FastAPI application
-app = FastAPI()
+# Defining a FastAPI router
+router = APIRouter()
 
 
-@app.get("/")
-async def welcome() -> dict:
-    return {"message": "Welcome to the API"}
+@router.get("/hello")
+async def say_hello() -> dict:
+    return {"message": "Hello"}
