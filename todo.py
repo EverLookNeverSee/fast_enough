86d67baa32ff8ab9a9bbdde368f21b3fb1e941f8
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-02 11:25:34 +0430

    Defining get_single_todo

--- todo.py
+++ todo.py
@@ -2,7 +2,7 @@
     todo.py - A simple todo list application
 """
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
 from model import Todo
 
 
@@ -21,3 +21,11 @@ async def add_todo(todo: Todo) -> dict:
 @todo_router.get("/todo")
 async def retrieve_todos() -> dict:
     return {"todos": todo_list}
+
+
+@todo_router.get("/todo/{todo_id}")
+async def get_single_todo(todo_id: int = Path(..., title="Todo ID", gt=0)) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return {"todo": todo}
+    return {"message": "Todo not found"}
