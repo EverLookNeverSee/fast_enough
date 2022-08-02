9a73eb90414c58d6df23fc407b6080668a65dbb5
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-02 11:36:45 +0430

    Defining example class for Todo

--- model.py
+++ model.py
@@ -13,3 +13,11 @@ class Item(BaseModel):
 class Todo(BaseModel):
     id: int
     item: Item
+
+    class Config:
+        Schema_extra = {
+            "Example": {
+                "id": 1,
+                "item": {"item": "Buy milk", "status": "pending"},
+            }
+        }
