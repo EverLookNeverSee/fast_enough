9d093e9d7cd6d5a56104481dbd72171408a5b756
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-02 10:55:06 +0430

    Implementing todo router

--- todo.py
+++ todo.py
@@ -3,3 +3,20 @@
 """
 
 from fastapi import APIRouter
+
+
+# Defining todo router
+todo_router = APIRouter()
+
+todo_list = list()
+
+
+@todo_router.post("/todo")
+async def add_todo(todo: dict) -> dict:
+    todo_list.append(todo)
+    return {"message": "Todo added successfully"}
+
+
+@todo_router.get("/todo")
+async def retrieve_todos() -> dict:
+    return {"todos": todo_list}
